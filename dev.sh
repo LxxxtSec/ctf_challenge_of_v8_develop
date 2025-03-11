70388865e58c243a2622dac62272f62ea295caa2
Lxxxt <1409080135@qq.com>
2025-03-11 14:56:36 +0800

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
-cd v8
+cd ./source_v8/v8
 git reset --hard $1
 git apply $2
 gclient sync -D
-gn gen out/x64_ctf.release --args='v8_monolithic=true v8_use_external_startup_data=false is_component_build=false is_debug=false target_cpu="x64" use_goma=false goma_dir="None" v8_enable_backtrace=true v8_enable_disassembler=true v8_enable_object_print=true v8_enable_verify_heap=true'
-ninja -c out/x64_ctf.release d8
+gn gen out/x64_ctf.debug --args='v8_monolithic=true v8_use_external_startup_data=false is_component_build=false is_debug=false target_cpu="x64" use_goma=false goma_dir="None" v8_enable_backtrace=true v8_enable_disassembler=true v8_enable_object_print=true v8_enable_verify_heap=true'
+ninja -C out/x64_ctf.debug d8
